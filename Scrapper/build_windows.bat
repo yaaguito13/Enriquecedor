8d04c368512f56323269b6609b7f8ee409a0edd7
Yago Pazos Lema <yagopazoslema@MacBook-Air-de-Yago.local>
2026-05-22 11:39:07 +0200

    Modificada creacion de .exe

--- Scrapper/build_windows.bat
+++ Scrapper/build_windows.bat
@@ -3,11 +3,10 @@ echo ========================================================
 echo Constructor del Ejecutable EmpresaScout para Windows
 echo ========================================================
 echo.
-echo Asegurate de tener Python instalado y haber ejecutado:
-echo pip install pyinstaller flask requests beautifulsoup4 lxml openpyxl duckduckgo-search
-echo.
-pause
+echo Instalando dependencias automaticamente...
+pip install pyinstaller flask requests beautifulsoup4 lxml openpyxl duckduckgo-search
 
+echo.
 echo Construyendo EmpresaScout.exe...
 cd src
 pyinstaller --noconfirm --onedir --windowed --icon=NONE --add-data "templates;templates/" --add-data "static;static/" --name "EmpresaScout" app.py
@@ -15,6 +14,7 @@ pyinstaller --noconfirm --onedir --windowed --icon=NONE --add-data "templates;te
 echo.
 echo ========================================================
 echo ¡Terminado!
-echo Tu programa se encuentra en la carpeta src/dist/EmpresaScout/
+echo Si todo ha ido bien, se ha creado la carpeta "dist" dentro de "src".
+echo Ruta exacta: src\dist\EmpresaScout\
 echo ========================================================
 pause
