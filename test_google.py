9f793569d5d1c9b1274f8935d2739301201cde39
yaaguito13 <yagopazoslema1@gmail.com>
2026-06-05 12:23:40 +0200

    Delete test_google.py

--- test_google.py
+++
@@ -1,7 +0,0 @@
-from googlesearch import search
-try:
-    results = search("Restaurante El Celler de Can Roca Girona", num_results=5, sleep_interval=2)
-    for res in results:
-        print(res)
-except Exception as e:
-    print("Error:", e)
